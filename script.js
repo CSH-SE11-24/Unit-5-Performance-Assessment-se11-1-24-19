7ee95899ed44eed281b0d5e97da29ea892c635f7
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-08 15:21:02 -0400

    Initial Commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let dub=document.querySelector(".do")
+dub.addEventListener("click",function
+(event){
+  document.body.style.background="red"
+})
+let dog=
+  document.querySelector(".dodo")
+dog.addEventListener("click",function
+(event){
+  document.body.style.background="blue"
+})
+let button=
+  document.querySelector("#helloButton")
+console.log(button)
+let tag=
+  document.querySelector("#helloText")
+console.log(tag)
